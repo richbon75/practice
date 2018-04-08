43cf36d76348fbe1018d292d2d6e965598855d81
Rich Boniface <richbon@gmail.com>
2018-04-08 13:38:08 -0400

    split advent_of_code_2015 files off into their own repository

--- advent_of_code_2015/a04_stocking.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-stub = b'iwrupvqb'
-
-for x in range(0, 1000000000):
-    if hashlib.md5(stub + bytes(str(x),'utf-8')).hexdigest()[0:5] == '00000':
-        print(x)
-        break
